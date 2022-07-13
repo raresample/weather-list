ddd2ff9815e4c46b99eb724495bd81cfa32dad1a
Aaron Hope <aghopecode@gmail.com>
2022-07-13 02:23:18 -0700

    Tweaked WeatherCard box-shadow

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -74,14 +74,15 @@ export default {
 .weather {
   background: white;
   /* color: #2c3e50; */
-  border-radius: 5px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  /* border-radius: 5px; */
+  /* box-shadow: 0 0 3px rgba(0,0,0,0.25); */
   margin: 1rem 0;
   padding: 1rem;
   display: grid;
   grid-template-columns: repeat(2, minmax(0, 1fr));
   height: 7.5rem;
   align-items: stretch;
+  box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 20%), 0 1px 5px 0 rgb(0 0 0 / 12%);
 }
 
 .left {
